d6bde4530bb45e85daa4652479421461267199af
Delph <49454418+Delph@users.noreply.github.com>
2021-09-11 21:19:47 +0100

    got it the right way up, but not quite right still

--- extra/fonttest.py
+++ extra/fonttest.py
@@ -1,10 +1,3 @@
-# glyph = [
-# 0b00011100,
-# 0b00000000,
-# 0b00000000,
-# 0b00000000,
-# 0b01000001
-# ]
 glyph = [
 0b01111100,
 0b00010010,
@@ -25,40 +18,40 @@ class TextCommand:
 
   def process(self, data, x, y):
     # out of x-bounds
-    if x < self.x or x >= self.x + (7 + 1) * len(self.text):
+    if x < self.x or x >= self.x + (5 + 1) * len(self.text):
       # print(f'out of x-bounds: x; {x}, self.x: {self.x}')
       return data
 
     # out of y-bounds
-    if y < self.y or y >= self.y + 5:
+    if y < self.y or y >= self.y + 7:
       # print(f'out of y-bounds: y; {y}, self.y: {self.y}')
       return data
 
     # 1px letter spacing
-    if (x - self.x + 1) % (7 + 1) == 0:
+    if (x - self.x + 1) % (5 + 1) == 0:
       return data
 
     print(f'{x}, {y}')
     print(f'org: {format(data, "08b")}')
 
-    index = (x - self.x) // (7 + 1)
+    index = (x - self.x) // (5 + 1)
     print(index)
     if index >= len(self.text):
       return data
     c = self.text[index]
 
-    glyph_slice = glyph[(y - self.y) % (5)]
+    glyph_slice = glyph[(x - self.x) % (5)]
     print(f'gls: {format(glyph_slice, "08b")}')
-    print(f'sel: {format(1 << (((x - self.x) % (7 + 1))), "08b")}')
-    print(f'bit: {format(glyph_slice & (1 << (((x - self.x) % (7 + 1)))), "08b")} {"<" if glyph_slice & (1 << ((x - self.x) % (7 + 1))) else ""}')
-    data = data | ((glyph_slice & (1 << (((x - self.x) % (7 + 1))))) << (self.x % (7 + 1))) % 0xff
+    print(f'sel: {format(1 << (((y - self.y) % (7 + 1))), "08b")}')
+    print(f'bit: {format(glyph_slice & (1 << (((y - self.y) % (7 + 1)))), "08b")} {"<" if glyph_slice & (1 << ((y - self.y) % (7 + 1))) else ""}')
+    data = data | (((glyph_slice & (1 << (((y - self.y) % (7 + 1))))) << ((8 + (x - self.x) - (y - self.y)) % (7 + 1))) % 0xff)
     print(f'dat: {format(data, "08b")}')
 
     return data
 
 
-tc = TextCommand(0, 0, 'AA')
-WIDTH = 16
+tc = TextCommand(1, 0, 'A')
+WIDTH = 8
 HEIGHT = 8
 buf = [0 for _ in range(((WIDTH+1) // 2) * HEIGHT)]
 
