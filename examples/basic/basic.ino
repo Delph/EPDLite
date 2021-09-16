7b7b2a38a565046e8800aa68cfe89076aeba9184
Delph <49454418+Delph@users.noreply.github.com>
2021-09-16 08:25:11 +0100

    update basic.ino

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -1,6 +1,7 @@
 #include <EPDLite.h>
 
 #include <EPDLite/fonts/font3x5.h>
+#include <EPDLite/fonts/font5x7.h>
 
 const uint8_t EPD_CS = 4;
 const uint8_t EPD_DC = 5;
@@ -8,18 +9,19 @@ const uint8_t EPD_BUSY = 6;
 const uint8_t EPD_RST = 7;
 
 EPDLite epd(152, 296, EPD_CS, EPD_DC, EPD_BUSY, EPD_RST);
+CommandBuffer<16> buffer;
 
 void setup()
 {
   epd.init();
 
-  CommandBuffer<10> buffer;
   buffer.push(LineCommand(10, 10, 20, 10));
   buffer.push(LineCommand(20, 10, 20, 20));
   buffer.push(RectCommand(50, 50, 20, 20));
   buffer.push(PixelCommand(50, 150));
   buffer.push(CircleCommand(50, 150, 20));
-  buffer.push(TextCommand(50, 200, "Hello world!", font3x5));
+  buffer.push(TextCommand(0, 40, "ABCDE", font5x7, 1));
+  buffer.push(TextCommand(0, 56, "abcde", font5x7, 1));
   epd.render(buffer);
 }
 
