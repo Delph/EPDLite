e3b3bf9445b199eef78273235a1b9c53b3b39a67
Delph <49454418+Delph@users.noreply.github.com>
2021-09-20 22:51:51 +0100

    update basic example

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -1,6 +1,5 @@
 #include <EPDLite.h>
 
-#include <EPDLite/fonts/font3x5.h>
 #include <EPDLite/fonts/font5x7.h>
 
 const uint8_t EPD_CS = 4;
@@ -9,19 +8,23 @@ const uint8_t EPD_BUSY = 6;
 const uint8_t EPD_RST = 7;
 
 EPDLite epd(152, 296, EPD_CS, EPD_DC, EPD_BUSY, EPD_RST);
-CommandBuffer<16> buffer;
+CommandBuffer<9> buffer;
+
 
 void setup()
 {
   epd.init();
+  epd.setOrientation(1);
 
   buffer.push(LineCommand(10, 10, 20, 10));
   buffer.push(LineCommand(20, 10, 20, 20));
-  buffer.push(RectCommand(50, 50, 20, 20));
-  buffer.push(PixelCommand(50, 150));
-  buffer.push(CircleCommand(50, 150, 20));
-  buffer.push(TextCommand(0, 40, "ABCDE", font5x7, 1));
-  buffer.push(TextCommand(0, 56, "abcde", font5x7, 1));
+  buffer.push(RectCommand(50, 50, 20, 30));
+  buffer.push(PixelCommand(276, 132));
+  buffer.push(CircleCommand(296, 152, 20));
+  buffer.push(TextCommand(20, 0, "ABCDEFGHIJKLM", font5x7, 1));
+  buffer.push(TextCommand(40, 8, "NOPQRSTUVWXYZ", font5x7, 1));
+  buffer.push(TextCommand(60, 16, "abcdefghijklm", font5x7, 2));
+  buffer.push(TextCommand(80, 32, "nopqrstuvwxyz", font5x7, 2));
   epd.render(buffer);
 }
 
