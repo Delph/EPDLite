5ec8f93d7cbb192d084181de51b81604d3c0e481
Delph <49454418+Delph@users.noreply.github.com>
2021-12-05 00:34:49 +0000

    some changes from debugging

--- extra/main.cpp
+++ extra/main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
+#include <iomanip>
 
 #include "stub.h"
 #include "../src/EPDLite/commands.h"
 #include "../src/EPDLite/fonts/font5x7.h"
 
-const int16_t WIDTH = 152;
-const int16_t HEIGHT = 50;
+const int16_t WIDTH = 24;
+const int16_t HEIGHT = 24;
 
 int main()
 {
   EPDLite epd(WIDTH, HEIGHT);
-  epd.setOrientation(2);
+  epd.setOrientation(1);
 
-  TextCommand* commands[] = {
-    new TextCommand(WIDTH/2, 8, "Hello World", font5x7, 1),
+  RectCommand* commands[] = {
+    new RectCommand(2, 6, 2, 4, true),
+    // new TextCommand(0, 8, "l", font5x7, 1),
+    // new TextCommand(8, 16, "l", font5x7, 2),
   };
-  uint8_t buf[((WIDTH+1) / 2) * HEIGHT] = {0};
+  uint8_t buf[((WIDTH+1) / 8) * HEIGHT] = {0};
 
   for (int16_t y = 0; y < HEIGHT; ++y)
   {
@@ -24,12 +27,18 @@ int main()
       buf[y * WIDTH / 8 + x / 8] = 0xff;
       for (int16_t xi = 0; xi < 8; ++xi)
       {
-        for (TextCommand* command : commands)
-          buf[y * WIDTH / 8 + (x + xi) / 8] = TextCommand::process(command, buf[y * WIDTH / 8 + x / 8], x + xi, y, epd);
+        if (x + xi >= WIDTH)
+          break;
+
+        for (RectCommand* command : commands)
+          buf[y * WIDTH / 8 + (x + xi) / 8] = RectCommand::process(command, buf[y * WIDTH / 8 + x / 8], x + xi, y, epd);
       }
     }
   }
 
+  for (int16_t x = 5; x < WIDTH; x += 5)
+    std::cout << std::setw(10) << x;
+  std::cout << std::endl;
   for (int16_t y = 0; y < HEIGHT; ++y)
   {
     for (int16_t x = 0; x < WIDTH; x += 8)
@@ -41,12 +50,16 @@ int main()
           std::cout << "#";
         else
           std::cout << ".";
+        std::cout << " ";
       }
     }
     std::cout << std::endl;
   }
+  for (int16_t x = 5; x < WIDTH; x += 5)
+    std::cout << std::setw(10) << x;
+  std::cout << std::endl;
 
-  for (TextCommand* tc : commands)
+  for (RectCommand* tc : commands)
     delete tc;
 }
 
